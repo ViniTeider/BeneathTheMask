539852881b9bd47ab3c5b77d673678448b31621e
Franklin <franklin.vieira@pucpr.edu.br>
2026-01-31 18:12:14 -0300

    mask item hover effect

--- Scripts/GenericPickup.gd
+++ Scripts/GenericPickup.gd
@@ -3,7 +3,12 @@ extends Area2D
 
 signal picked_up(mask)
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 var mask_type_to_give = Enum.MaskType.Melee
+
+func _ready() -> void:
+	animation_player.play("hover")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("change_mask"):
