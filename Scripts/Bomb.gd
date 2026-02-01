57d18691c65a8c1eaaaacad17b1a70eeab4f49aa
ViniTeider <vini469teider@gmail.com>
2026-02-01 10:15:50 -0300

    add rotation to uranium

--- Scripts/Bomb.gd
+++ Scripts/Bomb.gd
@@ -3,11 +3,15 @@ extends RigidBody2D
 
 @onready var timer: Timer = $Timer
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 # Do not delete
 var speed: float = 50.0
 var parent_direction: float = 0
 var player_id: int
+
+func _process(delta) -> void:
+	sprite_2d.rotation_degrees += 10 * sign(speed)
 
 func on_explodion() -> void:
 	animation_player.play("bomb_exploding")
